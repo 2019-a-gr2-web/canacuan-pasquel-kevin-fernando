afe6df391bf1c2a171b5e4bb133a46d3701a6fb1
KevinCanacuanPasquel <kfc0_10@hotmail.com>
2019-04-30 10:49:37 -0500

    COOKIE SEGURA

--- Deberes/01-calculadora/src/app.controller.ts
+++ Deberes/01-calculadora/src/app.controller.ts
@@ -18,20 +18,77 @@ export class AppController {
                  @Response() response,
                  @Request() request) {
     if (headers.numerouno && headers.numerodos) {
-      const cookie = request.cookies;
-      const numero1 = Number(headers.numerouno);
-      const numero2 = Number(headers.numerodos);
-      const totalSuma = numero1 + numero2;
-      response.cookie('nombreUsuario', 'Kevin');
-      return response.send({resultado: totalSuma.toString(),
-        nombreUsuario: cookie.nombreUsuario});
-    } else {
+      const cookies = request.cookies;
+      const cookieSig = request.signedCookies;
+      if (!cookieSig.puntos) {
+        response.cookie('puntos', 100, { signed: true });
+      }
+      const n1 = Number(headers.numero1);
+      const n2 = Number(headers.numero2);
+      if (!cookies.usuario) {
+        response.cookie('usuario', 'Kevin');
+      }
+      const suma = n1 + n2;
+      const tIntentos = cookieSig.puntos - suma;
 
-     return response.status(400).send({
-       mensajeError: 'ERROR... no existen numeros para SUMAR',
-       error: 400,
-     });
+      if (tIntentos <= 0) {
+        const res = {
+          resultado: `Resultado de la suma es: ${suma}`,
+          usuario: `Usuario: ${cookies.usuario}`,
+          mensaje: `Se acabaron los puntos`};
+        response.cookie('puntos', tIntentos, { signed: true });
+        response.send(res);
+      } else {
+        const res = {
+          resultado: `Resultado de la suma es: ${suma}`,
+          usuario: `Usuario: ${cookies.usuario}`,
+          mensaje: `Tiene ${tIntentos} puntos disponibles `};
+        response.cookie('puntos', tIntentos, { signed: true });
+        return response.send(res);
+      }
+    } else {
+      return response.status(400).send({
+        mensajeError: 'ERROR... no existen numeros para SUMAR',
+        error: 400,
+      });
     }
+    /*
+  if (headers.numerouno && headers.numerodos) {
+    //const cookie = request.cookies;
+    const cookieSegura = request.signedCookies;
+
+    if (!cookieSegura.intentos) {
+      response.cookieSegura('valor', 100, {signed: true});
+    }
+    const numero1 = Number(headers.numerouno);
+    const numero2 = Number(headers.numerodos);
+    const totalSuma = numero1 + numero2;
+
+    if (totalSuma <= cookieSegura.valor) {
+      response.cookieSegura('valor', cookieSegura.valor - totalSuma, {signed: true})
+      if (cookieSegura.valor == 0) {
+        response.cookieSegura('nombreUsuario', 'Kevin', {signed: true});
+        return response.send({resultado: totalSuma.toString(),
+          nombreUsuario: cookieSegura.nombreUsuario,
+          mensaje: 'Se le agotaron los puntos'});
+      } else {
+        response.cookieSegura('nombreUsuario', 'Kevin', {signed: true});
+        return response.send({resultado: totalSuma.toString(),
+          nombreUsuario: cookieSegura.nombreUsuario,
+          mensaje: `Le quedan: ${cookieSegura.valor} puntos`});
+      }
+    } else {
+      response.cookieSegura('nombreUsuario', 'Kevin', {signed: true});
+      return response.send({resultado: totalSuma.toString(),
+        nombreUsuario: cookieSegura.nombreUsuario,
+        mensaje: 'Excede los puntos'});
+    }
+  } else {
+   return response.status(400).send({
+     mensajeError: 'ERROR... no existen numeros para SUMAR',
+     error: 400,
+   });
+  } */
   }
 
   @Post('/restarDosNumeros')
@@ -39,6 +96,43 @@ export class AppController {
   restaDosNumeros(@Body() bodyParams,
                   @Response() response,
                   @Request() request) {
+
+    if (bodyParams.numeroUno && bodyParams.numeroDos) {
+      const cookies = request.cookies;
+      const cookieSig = request.signedCookies;
+      if (!cookieSig.puntos) {
+        response.cookie('puntos', 100, { signed: true });
+      }
+      const n1 = Number(bodyParams.numeroUno);
+      const n2 = Number(bodyParams.numeroDos);
+      if (!cookies.usuario) {
+        response.cookie('usuario', 'Fernando');
+      }
+      const resta = n1 - n2;
+      const tIntentos = cookieSig.puntos - resta;
+
+      if (tIntentos <= 0) {
+        const res = {
+          resultado: `Resultado de la resta es: ${resta}`,
+          usuario: `Usuario: ${cookies.usuario}`,
+          mensaje: `Se acabaron los puntos`};
+        response.cookie('puntos', tIntentos, { signed: true });
+        response.send(res);
+      } else {
+        const res = {
+          resultado: `Resultado de la resta es: ${resta}`,
+          usuario: `Usuario: ${cookies.usuario}`,
+          mensaje: `Tiene ${tIntentos} puntos disponibles `};
+        response.cookie('puntos', tIntentos, { signed: true });
+        return response.send(res);
+      }
+    } else {
+      return response.status(400).send({
+        mensaje: 'ERROR... no existen numeros para RESTAR',
+        error: 400,
+      });
+    }
+    /*
     if (bodyParams.numeroUno && bodyParams.numeroDos) {
       const cookie = request.cookies;
       const numero1 = Number(bodyParams.numeroUno);
@@ -52,7 +146,7 @@ export class AppController {
         mensaje: 'ERROR... no existen numeros para RESTAR',
         error: 400,
       });
-    }
+    } */
   }
 
   @Put('/multiplicarDosNumeros')
@@ -60,6 +154,41 @@ export class AppController {
   multiplicacion(@Query() queryParams,
                  @Response() response,
                  @Request() request) {
+    if (queryParams.numeroUno && queryParams.numeroDos) {
+      const cookies = request.cookies;
+      const cookieSig = request.signedCookies;
+      if (!cookieSig.puntos) {
+        response.cookie('puntos', 100, { signed: true });
+      }
+      const n1 = Number(queryParams.numeroUno);
+      const n2 = Number(queryParams.numeroDos);
+      if (!cookies.usuario) {
+        response.cookie('usuario', 'KevCP');
+      }
+      const multiplicacion = n1 * n2;
+      const tIntentos = cookieSig.puntos - multiplicacion;
+
+      if (tIntentos <= 0) {
+        const res = {
+          resultado: `Resultado de la multiplicacion es: ${multiplicacion}`,
+          usuario: `Usuario: ${cookies.usuario}`,
+          mensaje: `Se acabaron los puntos`};
+        response.cookie('puntos', tIntentos, { signed: true });
+        response.send(res);
+      } else {
+        const res = {
+          resultado: `Resultado de la multiplicacion es: ${multiplicacion}`,
+          usuario: `Usuario: ${cookies.usuario}`,
+          mensaje: `Tiene ${tIntentos} puntos disponibles `};
+        response.cookie('puntos', tIntentos, { signed: true });
+        return response.send(res);
+      }
+    } else {
+      return response.status(400).send({
+        mensaje: 'ERROR... no existen numeros para MULTIPLICAR',
+        error: 400,
+      });
+    }     /*
     if (queryParams.numeroUno && queryParams.numeroDos) {
       const cookie = request.cookies;
       const numero1 = Number(queryParams.numeroUno);
@@ -73,7 +202,7 @@ export class AppController {
         mensaje: 'ERROR... no existen numeros para MULTIPLICAR',
         error: 400,
       });
-    }
+    }*/
   }
 
   @Delete('/dividirDosNumeros')
@@ -85,14 +214,35 @@ export class AppController {
     @Response() response,
     @Request() request,
   ) {
-    if (queryParams.numeroUno && bodyParams.numeroDos) {
-      const cookie = request.cookies;
-      const numero1 = Number(queryParams.numeroUno);
-      const numero2 = Number(bodyParams.numeroDos);
-      const totalDivision = numero1 / numero2;
-      response.cookie('nombreUsuario', 'KFCP');
-      return response.send({resultado: totalDivision.toString(),
-        nombreUsuario: cookie.nombreUsuario});
+    if (queryParams.numeroUno && bodyParams.numeroDos && Number((bodyParams.numeroDos) !== 0 )) {
+      const cookies = request.cookies;
+      const cookieSig = request.signedCookies;
+      if (!cookieSig.puntos) {
+        response.cookie('puntos', 100, { signed: true });
+      }
+      const n1 = Number(queryParams.numeroUno);
+      const n2 = Number(bodyParams.numeroDos);
+      if (!cookies.usuario) {
+        response.cookie('usuario', 'Go Dota');
+      }
+      const division = n1 / n2;
+      const tIntentos = cookieSig.puntos - division;
+
+      if (tIntentos <= 0) {
+        const res = {
+          resultado: `Resultado de la division es: ${division}`,
+          usuario: `Usuario: ${cookies.usuario}`,
+          mensaje: `Se acabaron los puntos`};
+        response.cookie('puntos', tIntentos, { signed: true });
+        response.send(res);
+      } else {
+        const res = {
+          resultado: `Resultado de la division es: ${division}`,
+          usuario: `Usuario: ${cookies.usuario}`,
+          mensaje: `Tiene ${tIntentos} puntos disponibles `};
+        response.cookie('puntos', tIntentos, { signed: true });
+        return response.send(res);
+      }
     } else {
       return response.status(400).send({
         mensaje: 'ERROR.. no existen numeros para DIVIDIR' +
@@ -101,4 +251,20 @@ export class AppController {
       });
     }
   }
+  /*
+  const cookie = request.cookies;
+  const numero1 = Number(queryParams.numeroUno);
+  const numero2 = Number(bodyParams.numeroDos);
+  const totalDivision = numero1 / numero2;
+  response.cookie('nombreUsuario', 'KFCP');
+  return response.send({resultado: totalDivision.toString(),
+    nombreUsuario: cookie.nombreUsuario});
+} else {
+  return response.status(400).send({
+    mensaje: 'ERROR.. no existen numeros para DIVIDIR' +
+      ' o se envio 0',
+    error: 400,
+  });
+}
+} */
 }
